55f32d507b17f6f4d44d3f2124fd052db560792a
GhostInCerulean <318883144+GhostInCerulean@users.noreply.github.com>
2026-09-19 15:53:42 +0200

    remove pgo binary

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,21 @@
 #![windows_subsystem = "windows"] // no console window on windows
 
+use std::env;
+use std::hint::black_box;
+use charist::assets::KJV;
+use charist::bibles::load_bible_from_bytes;
 use charist::run;
+use charist::search_index::BibleIndex;
 
 fn main() -> cosmic::iced::Result {
-    run()
+    let args = env::args().collect::<Vec<String>>();
+    if let Some(arg) = args.get(1) && arg == "pgo" {
+        let bible = load_bible_from_bytes(KJV).unwrap();
+        let index = BibleIndex::build(&bible).unwrap();
+        black_box(bible);
+        black_box(index);
+        Ok(())
+    } else {
+        run()
+    }
 }
